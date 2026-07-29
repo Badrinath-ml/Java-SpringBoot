51e37feb74831cefe2ac19940dafd3f3bcdc145f
Badrinath <badrinathreddy2416@gmail.com>
2026-07-29 08:44:29 +0000

    Refactor dependencies and update application properties for improved configuration

--- backend/src/main/java/jar/controller/St.java
+++ backend/src/main/java/jar/controller/St.java
@@ -1,4 +1,12 @@
 package jar.controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -10,7 +18,8 @@ public class St {
         res.put("status", 200);
         return res;
     }
-
+    // @Autowired
+    // StudentRepo db;
 @PostMapping()
 Map<Object, Object> stPost(@RequestBody Map<Object, Object> body) {
     Map<Object, Object> res = new HashMap<>();
--- backend/src/main/java/jar/index.java
+++ backend/src/main/java/jar/index.java
@@ -2,7 +2,6 @@ package jar;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import jar.dto.R;
@@ -19,10 +18,7 @@ public class index {
         return "Welcome to spring boot";
     }
 
-    @GetMapping("/api")
-    R f1(){
-        return new R();
-    }
+    
 
     @PostMapping("/api/sum/{n}")
     R f1(@PathVariable int n) {
